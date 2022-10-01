cf8c7b81872a75e688e24940f1328593d85c6626
Frankie Nguyen <binguyen221198@gmail.com>
2022-10-02 01:39:18 +0700

    commit day 3

--- js/home.js
+++ js/home.js
@@ -1,42 +1,46 @@
 import axiosClient from './api/axiosClient';
 import postApi from './api/postAPI';
-import { setTextParent, setThumnailParent } from './utils';
+import { setTextParent, setThumnailParent, truncateText } from './utils';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+// to use fromNow function
+dayjs.extend(relativeTime);
 
 function createPostElement(post) {
   if (!post) return;
-  try {
-    // find template
-    const postTemplate = document.getElementById('postItemTemplate');
-    if (!postTemplate) return;
+  // find template
+  const postTemplate = document.getElementById('postItemTemplate');
+  if (!postTemplate) return;
 
-    // clone template
-    const liElement = postTemplate.content.firstElementChild.cloneNode(true);
+  // clone template
+  const liElement = postTemplate.content.firstElementChild.cloneNode(true);
 
-    if (!liElement) return;
+  if (!liElement) return;
 
-    //update title, decscription, author, timeSpan, thumbnail
-    // const titleElement = liElement.querySelector('[data-id = "title"]');
-    // if (titleElement) titleElement.textContent = post.title;
-    setTextParent(liElement, '[data-id = "title"]', post.title);
+  //update title, decscription, author, timeSpan, thumbnail
+  // const titleElement = liElement.querySelector('[data-id = "title"]');
+  // if (titleElement) titleElement.textContent = post.title;
+  setTextParent(liElement, '[data-id = "title"]', post.title);
 
-    // const authorElement = liElement.querySelector('[data-id = "author"]');
-    // if (authorElement) authorElement.textContent = post.author;
-    setTextParent(liElement, '[data-id = "author"]', post.author);
+  // const authorElement = liElement.querySelector('[data-id = "author"]');
+  // if (authorElement) authorElement.textContent = post.author;
+  setTextParent(liElement, '[data-id = "author"]', post.author);
 
-    // const decsElement = liElement.querySelector('[data-id = "description"]');
-    // if (decsElement) decsElement.textContent = post.decscription;
-    setTextParent(liElement, '[data-id = "description"]', post.decscription);
+  // const decsElement = liElement.querySelector('[data-id = "description"]');
+  // if (decsElement) decsElement.textContent = post.decscription;
+  setTextParent(liElement, '[data-id = "description"]', truncateText(post.description, 100));
 
-    // const thumbnailElement = liElement.querySelector('[data-id = "thumbnail"]');
-    // if (thumbnailElement) thumbnailElement.src = post.imageUrl;
-    setThumnailParent(liElement, '[data-id = "thumbnail"]', post.imageUrl);
+  //calculate timespan
+  setTextParent(liElement, '[data-id = "timeSpan"]', ` - ${dayjs(post.updateAt).fromNow()}`);
 
-    //attach event
+  // const thumbnailElement = liElement.querySelector('[data-id = "thumbnail"]');
+  // if (thumbnailElement) thumbnailElement.src = post.imageUrl;
+  setThumnailParent(liElement, '[data-id = "thumbnail"]', post.imageUrl);
 
-    return liElement;
-  } catch (error) {
-    console.log('failed to create post item', error);
-  }
+  //attach event
+
+  return liElement;
 }
 
 function renderPostList(postList) {
